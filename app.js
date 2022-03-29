532b3992842efc241acbcc9bf9c68a362ab7fba4
Nate Gowins <ngowins@gmail.com>
2022-03-29 15:04:07 -0500

    Completed exercise

--- app.js
+++ app.js
@@ -2,3 +2,44 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+
+class Person {
+  constructor(name, pets, residence, hobbies) {
+    this.name = name;
+    (this.pets = pets), (this.residence = residence);
+    this.hobbies = hobbies;
+  }
+
+  info() {
+    console.log(
+      `${this.name} has ${this.pets} pets, lives in ${this.residence}, and enjoys ${this.hobbies}.`
+    );
+  }
+
+  greeting() {
+    console.log(`Hello ${this.name}!`);
+  }
+}
+
+class Coder extends Person {
+  constructor(name, pets, residence, hobbies) {
+    super(name, pets, residence, hobbies);
+
+    this.occupation = "Full Stack Web Developer";
+  }
+
+  greeting() {
+    console.log(`Hello ${this.name}! Welcome to TrueCoders.`);
+  }
+}
+
+let John = new Person("John", 3, "Birmingham, AL", [
+  "cycling",
+  " learning Javascript",
+]);
+let Jane = new Coder("Jane", 0, "Riverchase, AL", ["working out"]);
+
+John.greeting();
+John.info();
+Jane.greeting();
+Jane.info();
